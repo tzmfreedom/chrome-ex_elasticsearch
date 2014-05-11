6cff7c9d4063085c009c2c7d5065dca34658d123
mtajitsu <makoto_tajitsu@hotmail.co.jp>
2014-05-11 20:45:56 +0900

    refactoring

--- background.js
+++ background.js
@@ -1,50 +1,53 @@
-chrome.history.onVisited.addListener(function(result){
-    var storage_local = chrome.storage.local;
-    var SERVER_KEY = "es-server";
-    var INDEX_KEY = "es-index";
-    var TYPE_KEY = "es-type";
-    var getNotificationOptions = function(message){
-        return {
-            type : "basic",
-            title : "setting error",
-            message : message,
-            iconUrl : ""
-        };
+var storage_local = chrome.storage.local;
+var SERVER_KEY = "es-server";
+var INDEX_KEY = "es-index";
+var TYPE_KEY = "es-type";
+var getNotificationOptions = function(message){
+    return {
+        type : "basic",
+             title : "setting error",
+             message : message,
+             iconUrl : ""
     };
+};
 
-    //ローカルストレージの設定を取得
-    storage_local.get(function(item){
-        if (item[SERVER_KEY]){
-            //elastic-searchサーバにデータを送信        
-            var index = item[INDEX_KEY] ? item[INDEX_KEY] : "history";
-            var type = item[TYPE_KEY] ? item[TYPE_KEY] : "chrome";
-            $.ajax({
-                url: item[SERVER_KEY] + "/" + index + "/" + type,
-                data : JSON.stringify({"url": result.url}),
-                type : "POST",
-                contentType : "application/json"
-            }).done(function(result){
-                console.log(result);
-            }).fail(function(result){
-                //通信が失敗した場合はメッセージを出力
-                chrome.notifications.create(
-                    "es-error", 
-                    getNotificationOptions("Sending to elasticsearch-server is failed.\r\nPlease confirm settings."), 
-                    function(result) {
-                        console.log(result);
-                    }
-                );
-            });
-        } else {
-            //サーバの設定が完了していない場合はメッセージを出力
+var storageItem;
+//ローカルストレージの設定を取得
+storage_local.get(function(item){
+    storageItem = item;
+});
+
+chrome.history.onVisited.addListener(function(result){
+    if (storageItem[SERVER_KEY]){
+        //elastic-searchサーバにデータを送信        
+        $.ajax({
+            url: storageItem[SERVER_KEY] + "/" + 
+                (storageItem[INDEX_KEY] ? storageItem[INDEX_KEY] : "history") + "/" + 
+                (storageItem[TYPE_KEY] ? storageItem[TYPE_KEY] : "chrome"),
+            data : JSON.stringify({"url": result.url}),
+            type : "POST",
+            contentType : "application/json"
+        }).done(function(result){
+            console.log(result);
+        }).fail(function(result){
+            //通信が失敗した場合はメッセージを出力
             chrome.notifications.create(
                 "es-error", 
-                getNotificationOptions("You did not set up elasticsearch server information.\r\nPlease set up the information."), 
+                getNotificationOptions("Sending to elasticsearch-server is failed.\r\nPlease confirm settings."), 
                 function(result) {
                     console.log(result);
                 }
             );
-        }
-    });
+        });
+    } else {
+        //サーバの設定が完了していない場合はメッセージを出力
+        chrome.notifications.create(
+            "es-error", 
+            getNotificationOptions("You did not set up elasticsearch server information.\r\nPlease set up the information."), 
+            function(result) {
+                console.log(result);
+            }
+        );
+    }
 });
 
